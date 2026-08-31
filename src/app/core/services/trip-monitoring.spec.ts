bfe9a429084be53055699bf5726586d46788c3ef
Tigistu12 <tigistu1221@gmail.com>
2026-08-31 12:51:04 +0300

    updated

--- src/app/core/services/trip-monitoring.spec.ts
+++ src/app/core/services/trip-monitoring.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-import { TripMonitoring } from './trip-monitoring';
+// import { TripMonitoring } from './trip-monitoring';
+import { TripMonitoringService } from './trip-monitoring.service';
+
 
 describe('TripMonitoring', () => {
-  let service: TripMonitoring;
+  let service: TripMonitoringService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(TripMonitoring);
+    service = TestBed.inject(TripMonitoringService);
   });
 
   it('should be created', () => {
